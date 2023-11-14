b929d8151fe093174766bfe7e2dc9caeac20a6d0
Speed Force <122682099+speedforce-lk@users.noreply.github.com>
2023-11-14 11:47:51 +0530

    Update node.sh

--- node.sh
+++ node.sh
@@ -10,8 +10,3 @@ cd Marzban-node
 curl -fsSL https://get.docker.com | sh
 #Run Docker
 docker compose up -d
-# Sleep
-sleep 10
-#Upload Pem File
-curl --upload-file /var/lib/marzban-node/ssl_cert.pem https://transfer.sh/ssl_cert.pem
-#End
